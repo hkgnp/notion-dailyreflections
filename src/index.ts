a64f254211c0864c06b6ce49e14f5b6a1ef78c8d
hkgnp <ben@pngs.cc>
2023-12-03 15:08:49 +0800

    feat: add docker

--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,22 @@ import dotenv from "dotenv";
 import { getCreighton } from "./get-creighton";
 import { getGospel } from "./get-gosptel";
 import { createNotionPage } from "./create-notion-page";
+import { CronJob } from "cron";
 
 dotenv.config();
 
 const main = async () => {
   const creighton = await getCreighton();
   const gospel = await getGospel();
-
   await createNotionPage(creighton, gospel);
 };
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+new CronJob(
+  "0 6 * * *",
+  async function () {
+    await main();
+  },
+  null,
+  true,
+  "Asia/Singapore",
+);
